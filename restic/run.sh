596db607f449993dbeb6c3b8a2200f2a2e5d9c3b
Marten Jacobs <martenjacobs@users.noreply.github.com>
2021-01-21 14:36:17 +0100

    Pre Release

--- restic/run.sh
+++ restic/run.sh
@@ -1,29 +1,28 @@
 #!/usr/bin/with-contenv bashio
-
-set -e
+echo "HassOS restic add-on starting"
+date
+set -x
 
 mkdir -p /data/restic-cache
 
-# find and mount the data partition
-udevd --daemon && sleep 0.5
-udevadm trigger && sleep 0.5
-mkdir /hassos-data
-mount /dev/disk/by-label/hassos-data /hassos-data/
-
 # load configuration
 CONFIG_PATH=/data/options.json
-$(jq -r \
-  '.env_vars|to_entries[]|"export " + .key + "=" + (.value|tostring) + ""' \
-  "$CONFIG_PATH")
-jq -r \
-  '.exclude_patterns[]|.' \
-  "$CONFIG_PATH" > /exclude_file
+
+$(jq -r '.env_vars|to_entries[]|"export " + .key + "=" + (.value|tostring) + ""' "$CONFIG_PATH")
+jq -r '.exclude_patterns[]|.' "$CONFIG_PATH" > /exclude_file
+
+restic version
+restic stats
+
 # run the backup
-(
-  cd /hassos-data/supervisor
-  restic \
-    --cache-dir=/data/restic-cache \
-    --verbose \
-    --exclude-file=/exclude_file \
-    backup .
-)
+restic \
+  --cache-dir=/data/restic-cache \
+  --verbose \
+  --exclude-file=/exclude_file \
+  backup           \
+      /backup/      \
+      /ssl/         \
+      /config/
+
+
+echo "Done"
